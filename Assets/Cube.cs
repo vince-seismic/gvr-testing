4fd14bfeab76052f2fd88165f62c402c74a66f29
Vince Fennel <vfennel@seismicgames.com>
2016-09-01 17:57:08 -0700

    initial commit

---
+++ Assets/Cube.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+	
+	void Update () {
+		gameObject.transform.Rotate (1, 1, 0);
+	}
+}
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour {
+
+	private LineRenderer line;
+
+	void Start () {
+		line = gameObject.GetComponent<LineRenderer> ();
+		line.enabled = false;
+	}
+
+	void Update () {
+		if (GvrController.TouchDown == true) {
+			//shoot laser
+			StopCoroutine("FireLaser");
+			StartCoroutine ("FireLaser");
+		}
+	}
+
+	IEnumerator FireLaser () {
+		line.enabled = true;
+		while (GvrController.IsTouching == true) {
+			Ray ray = new Ray (transform.position, transform.forward);
+
+			line.SetPosition (0, ray.origin);
+			line.SetPosition (1, ray.GetPoint (100));
+
+			yield return null;
+		}
+
+		line.enabled = false;
+	}
+}
